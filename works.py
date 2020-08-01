1f970314907c7605eb261138478279f6421b04a9
skylacking04 <rudo@bigbrain.ai>
2020-08-01 00:31:53 +0000

    works.py is calling from external

--- works.py
+++ works.py
@@ -1,20 +1,24 @@
 import json
 
 
-
 with open("airports.json", encoding='utf-8', errors='ignore') as json_data:
     data = json.load(json_data, strict=False)
 
+given_airport_code = input("What is your airport code? ")
 
-airport_code = "SFO"
+# save the answer to a variable
+
+
+print("your airport code is ", given_airport_code)
+airport_code = given_airport_code
 # print(data['airport'][0]['location'])
 # airport_code = given_airport_code
 
 for i in data['airport']:
-    print(i['airport-code'])
+    # print(i['airport-code'])
     try:
         if (airport_code == i['airport-code']) or (airport_code == i['airport-code-2']) or  (airport_code == i['airport-code-3'])  or  (airport_code == i['airport-code-4']):
-            print(i['location'])
+            # print(i['location'])
             new_loc = i['location']
             print("Your Location is ", new_loc)
             break
